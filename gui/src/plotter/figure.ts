f5230c150f764fca2f41f61848362319c5a5feb4
Dominik Madea <dominik.madea@seznam.cz>
2023-11-12 20:54:07 +0900

    colormap class

--- gui/src/plotter/figure.ts
+++ gui/src/plotter/figure.ts
@@ -3,6 +3,7 @@ import { GraphicObject } from "./object";
 import { Rect, NumberArray, Point, Margin, Matrix } from "./types";
 import { backgroundColor, fontSizeLabels, fontSizeNumbers, frameColor, textColor } from "./settings";
 import { Dataset, formatNumber } from "./utils";
+import { Colormap, IColorMap } from "./colormap";
 
 interface IFigureSettings {
     xAxis: {
@@ -33,7 +34,7 @@ interface ILinePlot {
 
 interface IHeatMap {
     dataset: Dataset,
-    colormap: string,
+    colormap: IColorMap,
     iData: ImageData
 }
 
@@ -291,58 +292,6 @@ export class Figure extends GraphicObject {
         return plot;
     }
 
-    private getRGBA(pos: number): number[] {
-
-        var colormap = [
-            {pos: 0, r: 255, g: 255, b: 255, a: 255},
-            {pos: 0.5, r: 255, g: 255, b: 255, a: 255},
-            {pos: 1, r: 0, g: 160, b: 0, a: 255}
-        ]
-
-        var sym_grad =  [
-            {pos: 0, r: 75, g: 0, b: 130, a: 255},
-            {pos: 0.333, r: 0, g: 0, b: 255, a: 255},
-            {pos: 0.5, r: 255, g: 255, b: 255, a: 255},
-            {pos: 0.625, r: 255, g: 255, b: 0, a: 255},
-            {pos: 0.75, r: 255, g: 165, b: 0, a: 255},
-            {pos: 0.875, r: 255, g: 0, b: 0, a: 255},
-            {pos: 1, r: 150, g: 0, b: 0, a: 255}
-        ]
-
-        // s tyrkysovou
-        // var sym_grad =  [
-        //     {pos: 0, r: 75, g: 0, b: 130, a: 255},
-        //     {pos: 0.29, r: 0, g: 0, b: 255, a: 255},
-        //     {pos: 0.38, r: 0, g: 255, b: 255, a: 255},
-        //     {pos: 0.5, r: 255, g: 255, b: 255, a: 255},
-        //     {pos: 0.625, r: 255, g: 255, b: 0, a: 255},
-        //     {pos: 0.75, r: 255, g: 165, b: 0, a: 255},
-        //     {pos: 0.875, r: 255, g: 0, b: 0, a: 255},
-        //     {pos: 1, r: 150, g: 0, b: 0, a: 255}
-        // ]
-
-        colormap = sym_grad;
-
-        pos = (pos < 0) ? 0 : pos;
-        pos = (pos > 1) ? 1 : pos;
-
-        for (var i = 0; i < colormap.length - 1; i++) {
-            if (colormap[i].pos === pos) break;
-            if (colormap[i].pos < pos && pos <= colormap[i + 1].pos){
-                break;
-            }
-        }
-
-        let diff = colormap[i+1].pos - colormap[i].pos;
-        let x = (pos - colormap[i].pos) / diff;
-        
-        return [x * colormap[i+1].r + (1 - x) * colormap[i].r,
-                x * colormap[i+1].g + (1 - x) * colormap[i].g,
-                x * colormap[i+1].b + (1 - x) * colormap[i].b,
-                x * colormap[i+1].a + (1 - x) * colormap[i].a]
-
-    }
-
     public redrawHeatmapOffCanvas(){
         if (!this.heatmap || !this.offScreenCanvas || !this.offScreenCanvasCtx) {
             return;
@@ -374,7 +323,7 @@ export class Figure extends GraphicObject {
                 // interpolate the rgba values
                 // console.log(zScaled);
 
-                let rgba = this.getRGBA(zScaled);
+                let rgba = Colormap.getColor(zScaled, this.heatmap.colormap);
 
                 iData.data[pos] = rgba[0];              // some R value [0, 255]
                 iData.data[pos+1] = rgba[1];              // some G value
@@ -387,7 +336,7 @@ export class Figure extends GraphicObject {
         // this.paint();
     }
 
-    public plotHeatmap(dataset: Dataset, colormap: string = "Femto"): IHeatMap | null {
+    public plotHeatmap(dataset: Dataset, colormap: IColorMap = Colormap.symgrad ): IHeatMap | null {
         let data = dataset.data;
 
         if (data.ncols !== dataset.x.length || data.nrows !== dataset.y.length) {
@@ -431,7 +380,7 @@ export class Figure extends GraphicObject {
             return;
         }
 
-        this.ctx.imageSmoothingEnabled = true;
+        this.ctx.imageSmoothingEnabled = false;
         this.redrawHeatmapOffCanvas();
 
 
@@ -440,8 +389,12 @@ export class Figure extends GraphicObject {
         let x = this.heatmap.dataset.x;
         let y = this.heatmap.dataset.y;
 
-        let p0 = this.mapRange2Canvas({x: x[0], y: y[0]});
-        let p1 = this.mapRange2Canvas({x: x[x.length - 1], y: y[y.length - 1]});
+        // for evenly spaced data
+        let xdiff = x[1] - x[0];
+        let ydiff = y[1] - y[0];
+
+        let p0 = this.mapRange2Canvas({x: x[0] - xdiff / 2, y: y[0] - ydiff / 2});
+        let p1 = this.mapRange2Canvas({x: x[x.length - 1] + xdiff / 2, y: y[y.length - 1] + ydiff / 2});
 
         this.ctx.drawImage(b, 
         0, 0, this.heatmap.iData.width, this.heatmap.iData.height,
