21dceb560946737d3ecdbd94030bf9c56d75fd5d
dominik madea <dominik.madea@seznam.cz>
2023-12-15 16:38:34 +0900

    layout control

--- gui/src/plotter/layoutscene.ts
+++ gui/src/plotter/layoutscene.ts
@@ -6,6 +6,11 @@ import { NumberArray } from "./types";
 
 
 export class LayoutSceneNavBar extends SceneNavBar {
+
+    constructor(scene: Scene) {
+        super(scene);
+    }
+
     protected setContextMenu() {
         this.contextMenu = new LayoutSceneNavBarContextMenu(this.scene);
     }
@@ -15,15 +20,31 @@ export class LayoutSceneNavBarContextMenu extends SceneNavBarContextMenu {
 
     protected constructMenu(): void {
         super.constructMenu();
+
+        const scene = this.scene as LayoutScene;
         
-        var ops = ["Femto", "Transient emission", "HPLC"]
+        var ops = ["Femto", "Transient emission", "HPLC"];
 
         var layout = this.addSelect("Layout", ...ops);
         layout.addEventListener("change", e => {
+            scene.layout = layout.selectedOptions[0].text;
+        });
 
-            // const opt = axAlign.selectedOptions[0].text;
-            // this.fig.axisAlignment = opt == "Vertical" ? Orientation.Vertical : Orientation.Horizontal;
-            // this.fig.repaint();
+        var ops = ["Matrix", "Columns-wise", "Row-wise"];
+        var alignment = this.addSelect("Alignment", ...ops);
+        alignment.addEventListener("change", e => {
+            scene.alignment = alignment.selectedOptions[0].text;
+        });
+
+        var linkHorizontalLine = this.addCheckBox("Link horizontal lines");
+        var linkVerticalLine = this.addCheckBox("Link vertical lines");
+
+        linkHorizontalLine.addEventListener("change", e => {
+
+        });
+
+        linkVerticalLine.addEventListener("change", e => {
+            
         });
 
         this.addUpdateUICallback(() => {
@@ -39,8 +60,10 @@ export class LayoutScene extends Scene {
     public heatmapFigures: Figure[] = [];
     public traceFigures: Figure[] = [];
     public spectraFigures: Figure[] = [];
-    public layout: string = "Femto";
-
+    public _layout: string = "Femto";
+    public _alignment: string = "Matrix";
+    public linkVerticalLines = false;
+    public linkHorizontalLines = false;
 
     constructor(parentElement: HTMLDivElement) {
         super(parentElement);
@@ -49,7 +72,7 @@ export class LayoutScene extends Scene {
         this.addItem(this.grid);
 
         var hfig = new Figure();
-        hfig.addColorbar();
+        // hfig.addColorbar();
         var trace = new Figure();
         var spectrum = new Figure();
 
@@ -60,45 +83,79 @@ export class LayoutScene extends Scene {
         this.arangeFigures();
     }
 
+    get layout() {
+        return this._layout;
+    }
+
+    set layout(layout: string) {
+        this._layout = layout;
+        this.arangeFigures();
+    }
+
+    get alignment() {
+        return this._alignment;
+    }
+
+    set alignment(alignment: string) {
+        this._alignment = alignment;
+        this.arangeFigures();
+    }
+
     protected arangeFigures() {
-        const n = this.heatmapFigures.length;
         this.grid.clear();
+        const n = this.heatmapFigures.length;
+        let nrows, ncols;
+        if (this._alignment === "Matrix") {
+            nrows = Math.floor(n ** 0.5);
+            ncols = Math.ceil(n / nrows);
+        } else if (this._alignment === "Column-wise") {
+            ncols = n;
+            nrows = 1;
+        } else { // row-wise
+            ncols = 1;
+            nrows = n;
+        }
 
         switch (this.layout) {
             case "Femto": {
-
-                for (let i = 0; i < n; i++) {
-                    const hmap = this.heatmapFigures[i];
-                    const sp = this.spectraFigures[i];
-                    const tr = this.traceFigures[i];
-
-                    hmap.showTickNumbers = ['left', 'bottom'];
-                    sp.showTickNumbers = ['left', 'bottom'];
-                    tr.showTickNumbers = ['right', 'bottom'];
-
-                    // display 
-                    this.grid.addItem(hmap, 0, 2 * i, 1, 1);
-                    this.grid.addItem(sp, 1, 2 * i, 1, 1);
-                    this.grid.addItem(tr, 0, 2 * i + 1, 1, 1);
+                for (let row = 0; row < nrows; row++) {
+                    for (let col = 0; col < ncols; col++) {
+                        const i = row * ncols + col;
+                        const hmap = this.heatmapFigures[i];
+                        const sp = this.spectraFigures[i];
+                        const tr = this.traceFigures[i];
+    
+                        hmap.showTickNumbers = ['left', 'bottom'];
+                        sp.showTickNumbers = ['left', 'bottom'];
+                        tr.showTickNumbers = ['right', 'bottom'];
+    
+                        // display 
+                        this.grid.addItem(hmap, 2 * row, 2 * col, 1, 1);
+                        this.grid.addItem(sp, 2 * row + 1, 2 * col, 1, 1);
+                        this.grid.addItem(tr, 2 * row, 2 * col + 1, 1, 1);
+                    }
                 }
-                var ratios = new NumberArray();
-                for (let i = 0; i < n; i++) {ratios.push(2); ratios.push(1);}
+                var wr = new NumberArray();
+                for (let i = 0; i < ncols; i++) {wr.push(2); wr.push(1);}
+                var hr = new NumberArray();
+                for (let i = 0; i < nrows; i++) {hr.push(2); hr.push(1);}
 
-                this.grid.gridSettings.widthRatios = ratios;
-                this.grid.gridSettings.heightRatios = ratios;
+                this.grid.gridSettings.widthRatios = wr;
+                this.grid.gridSettings.heightRatios = hr;
                 break;
             }
-            case "Femto": {
+            // case "Femto": {
 
 
-                break;
-            }
-            case "Femto": {
+            //     break;
+            // }
+            // case "Femto": {
 
 
-                break;
-            }
+            //     break;
+            // }
         }
+        this.grid.recalculateGrid();
         this.repaint();
         setTimeout(() => this.resize(), 100);
     }
--- gui/src/scene.ts
+++ gui/src/scene.ts
@@ -113,171 +113,187 @@ export class SceneUser extends LayoutScene {
     }
 
     private processDatasets(names: string[]) {
+        
+        // this.clear();
 
-        this.grid.clear();
-        this.figHeatmaps = [];
-        this.figSpecrum = null;
-        this.figTrace = null;
+        this.heatmapFigures = [];
+        this.traceFigures = [];
+        this.spectraFigures = [];
 
         const n = this.datasets.length;
 
-        var figy = new Figure();
-        // figy.xAxis.scale = 'lin';
-        figy.xAxis.symlogLinthresh = 2;
-        figy.xAxis.symlogLinscale = 2;
-        figy.xAxis.autoscale = false;
-        figy.yAxis.autoscale = true;    
-        this.figTrace = figy;
+        for (const ds of this.datasets) {
+            var trace = new Figure();
+            var spectrum = new Figure();
+            var h = new Figure();
+            var cbar = h.addColorbar();
+            var hmap = h.plotHeatmap(ds, new Colormap(Colormaps.symgrad));
+            cbar.linkHeatMap(hmap);
 
-        var figx = new Figure();
-        // figy.xAxis.scale = 'lin';
-        figx.xAxis.symlogLinthresh = 2;
-        figx.xAxis.symlogLinscale = 2;
-        figx.xAxis.autoscale = false;
-        figx.yAxis.autoscale = true;    
-        figx.xAxis.label = 'Wavelength / nm';
-        figy.xAxis.label = 'Time / ps';
-        figy.showTickNumbers = ['left', 'bottom'];
-        figx.showTickNumbers = ['left', 'bottom'];
-
-
-        this.figSpecrum = figx;
-
-        this.grid.addItem(figy, 1, 0, 1, n);
-        this.grid.addItem(figx, 2, 0, 1, n);
-
-        var ly = figy.addDraggableLines(DraggableLines.Orientation.Vertical);
-        var lx = figx.addDraggableLines(DraggableLines.Orientation.Vertical);
-
-        // var spectra: ILinePlot[] = Array<ILinePlot>(n);
-        // var traces: ILinePlot[] = Array<ILinePlot>(n);
-
-        // var linePlot = figx.plotLine(new NumberArray(), new NumberArray());
-        // var linePlotCol = figy.plotLine(new NumberArray(), new NumberArray());
-
-        figx.linkMargin(figy, Orientation.Vertical);
-
-        // ly.addPositionChangedListener((e) => {
-        //     if (this.fig?.heatmap) {
-        //         let idxy = this.fig.heatmap.dataset.y.nearestIndex(e.realPosition.x);
-        //         let row = this.fig.heatmap.dataset.data.getRow(idxy);
-
-        //         linePlot.y = row;
-
-        //         figx.repaint();
-        //     }
-        // });
-
-        // lx.addPositionChangedListener((e) => {
-        //     if (this.fig?.heatmap) {
-        //         let idxx = this.fig.heatmap.dataset.x.nearestIndex(e.realPosition.x);
-        //         let col = this.fig.heatmap.dataset.data.getCol(idxx);
-
-        //         linePlotCol.x = this.fig.heatmap.dataset.y;
-        //         linePlotCol.y = col;
-
-        //         figy.repaint();
-        //     }
-        // });
-
-        for (let i = 0; i < n; i++) {
-            // dataset.data.log();
-            const dataset = this.datasets[i];
-
-            var heatmapFig = new Figure();
-            this.heatmapFigures.push(heatmapFig);
-            heatmapFig.yAxis.inverted = true;
-            heatmapFig.yAxis.label = 'Time / ps';
-            heatmapFig.xAxis.label = 'Wavelength / nm';
-            heatmapFig.title = names[i];
-            heatmapFig.showTickNumbers = ['left', 'bottom'];
-            const heatmap = heatmapFig.plotHeatmap(dataset, new Colormap(Colormaps.symgrad));
-            this.colorbar = heatmapFig.addColorbar(new Colormap(Colormaps.symgrad));
-            this.colorbar.linkHeatMap(heatmap)
-            
-            this.grid.addItem(heatmapFig, 0, i);
-
-            let xdiff, ydiff, xOffset, yOffset;
-            
-            if (!heatmap.isXRegular) {
-                xdiff = 1;
-                xOffset = 0;
-            } else {
-                xdiff = (dataset.x[dataset.x.length - 1] - dataset.x[0]) / (dataset.x.length - 1);
-                xOffset = dataset.x[0];
-            }
-    
-            // y axis
-    
-            if (!heatmap.isYRegular) {
-                ydiff = 1;
-                yOffset = 0;
-            } else {
-                ydiff = (dataset.y[dataset.y.length - 1] - dataset.y[0]) / (dataset.y.length - 1);
-                yOffset = dataset.y[0];
-            }
-
-            var dLines = heatmapFig.addDraggableLines(DraggableLines.Orientation.Both);
-            this.dLinesArr.push(dLines);
-            dLines.setStickGrid(xdiff, xOffset, ydiff, yOffset);
-            heatmapFig.setViewBounds([dataset.x[0], dataset.x[dataset.x.length - 1]], [dataset.y[0], dataset.y[dataset.y.length - 1]]);
-
-            heatmapFig.linkXRange(figx);
-            heatmapFig.linkYXRange(figy);  
-            
-            const index = i;
-            const color = getDefaultColor(index);
-
-            const linePlot = figx.plotLine(new NumberArray(), new NumberArray(), color);
-            const linePlotCol = figy.plotLine(new NumberArray(), new NumberArray(), color);
-
-            dLines.addPositionChangedListener((e) => {
-
-                if (e.yChanged) {
-                        // get row
-                    let idxy = heatmap.dataset.y.nearestIndex(e.realPosition.y);
-                    let row = heatmap.dataset.data.getRow(idxy);
-                    linePlot.y = row;
-                    figx.repaint();
-                }
-
-                if (e.xChanged) {
-                    let idxx = heatmap.dataset.x.nearestIndex(e.realPosition.x);
-                    let col = heatmap.dataset.data.getCol(idxx);
-    
-                    if (linePlot.x.length !== heatmap.dataset.y.length) {
-                        linePlot.x = heatmap.dataset.x;
-                    }
-                    linePlotCol.x = heatmap.dataset.y;
-                    linePlotCol.y = col;
-                    figy.repaint();
-                }
-    
-            });
-    
-            dLines.linkX(lx);
-            dLines.linkYX(ly);
-            dLines.positionChanged(true, true);
+            this.heatmapFigures.push(h);
+            this.traceFigures.push(trace);
+            this.spectraFigures.push(spectrum);
         }
 
-        this.grid.gridSettings.heightRatios = NumberArray.fromArray([2, 1, 1]);
+        this.arangeFigures();
 
-        // combinations
-        for (let i = 0; i < this.figHeatmaps.length - 1; i++) {
-            for (let j = 1; j < this.figHeatmaps.length; j++) {
+        // var figy = new Figure();
+        // // figy.xAxis.scale = 'lin';
+        // figy.xAxis.symlogLinthresh = 2;
+        // figy.xAxis.symlogLinscale = 2;
+        // figy.xAxis.autoscale = false;
+        // figy.yAxis.autoscale = true;    
+        // this.figTrace = figy;
+
+        // var figx = new Figure();
+        // // figy.xAxis.scale = 'lin';
+        // figx.xAxis.symlogLinthresh = 2;
+        // figx.xAxis.symlogLinscale = 2;
+        // figx.xAxis.autoscale = false;
+        // figx.yAxis.autoscale = true;    
+        // figx.xAxis.label = 'Wavelength / nm';
+        // figy.xAxis.label = 'Time / ps';
+        // figy.showTickNumbers = ['left', 'bottom'];
+        // figx.showTickNumbers = ['left', 'bottom'];
+
+
+        // this.figSpecrum = figx;
+
+        // this.grid.addItem(figy, 1, 0, 1, n);
+        // this.grid.addItem(figx, 2, 0, 1, n);
+
+        // var ly = figy.addDraggableLines(DraggableLines.Orientation.Vertical);
+        // var lx = figx.addDraggableLines(DraggableLines.Orientation.Vertical);
+
+        // // var spectra: ILinePlot[] = Array<ILinePlot>(n);
+        // // var traces: ILinePlot[] = Array<ILinePlot>(n);
+
+        // // var linePlot = figx.plotLine(new NumberArray(), new NumberArray());
+        // // var linePlotCol = figy.plotLine(new NumberArray(), new NumberArray());
+
+        // figx.linkMargin(figy, Orientation.Vertical);
+
+        // // ly.addPositionChangedListener((e) => {
+        // //     if (this.fig?.heatmap) {
+        // //         let idxy = this.fig.heatmap.dataset.y.nearestIndex(e.realPosition.x);
+        // //         let row = this.fig.heatmap.dataset.data.getRow(idxy);
+
+        // //         linePlot.y = row;
+
+        // //         figx.repaint();
+        // //     }
+        // // });
+
+        // // lx.addPositionChangedListener((e) => {
+        // //     if (this.fig?.heatmap) {
+        // //         let idxx = this.fig.heatmap.dataset.x.nearestIndex(e.realPosition.x);
+        // //         let col = this.fig.heatmap.dataset.data.getCol(idxx);
+
+        // //         linePlotCol.x = this.fig.heatmap.dataset.y;
+        // //         linePlotCol.y = col;
+
+        // //         figy.repaint();
+        // //     }
+        // // });
+
+        // for (let i = 0; i < n; i++) {
+        //     // dataset.data.log();
+        //     const dataset = this.datasets[i];
+
+        //     var heatmapFig = new Figure();
+        //     this.heatmapFigures.push(heatmapFig);
+        //     heatmapFig.yAxis.inverted = true;
+        //     heatmapFig.yAxis.label = 'Time / ps';
+        //     heatmapFig.xAxis.label = 'Wavelength / nm';
+        //     heatmapFig.title = names[i];
+        //     heatmapFig.showTickNumbers = ['left', 'bottom'];
+        //     const heatmap = heatmapFig.plotHeatmap(dataset, new Colormap(Colormaps.symgrad));
+        //     this.colorbar = heatmapFig.addColorbar(new Colormap(Colormaps.symgrad));
+        //     this.colorbar.linkHeatMap(heatmap)
+            
+        //     this.grid.addItem(heatmapFig, 0, i);
+
+        //     let xdiff, ydiff, xOffset, yOffset;
+            
+        //     if (!heatmap.isXRegular) {
+        //         xdiff = 1;
+        //         xOffset = 0;
+        //     } else {
+        //         xdiff = (dataset.x[dataset.x.length - 1] - dataset.x[0]) / (dataset.x.length - 1);
+        //         xOffset = dataset.x[0];
+        //     }
+    
+        //     // y axis
+    
+        //     if (!heatmap.isYRegular) {
+        //         ydiff = 1;
+        //         yOffset = 0;
+        //     } else {
+        //         ydiff = (dataset.y[dataset.y.length - 1] - dataset.y[0]) / (dataset.y.length - 1);
+        //         yOffset = dataset.y[0];
+        //     }
+
+        //     var dLines = heatmapFig.addDraggableLines(DraggableLines.Orientation.Both);
+        //     this.dLinesArr.push(dLines);
+        //     dLines.setStickGrid(xdiff, xOffset, ydiff, yOffset);
+        //     heatmapFig.setViewBounds([dataset.x[0], dataset.x[dataset.x.length - 1]], [dataset.y[0], dataset.y[dataset.y.length - 1]]);
+
+        //     heatmapFig.linkXRange(figx);
+        //     heatmapFig.linkYXRange(figy);  
+            
+        //     const index = i;
+        //     const color = getDefaultColor(index);
+
+        //     const linePlot = figx.plotLine(new NumberArray(), new NumberArray(), color);
+        //     const linePlotCol = figy.plotLine(new NumberArray(), new NumberArray(), color);
+
+        //     dLines.addPositionChangedListener((e) => {
+
+        //         if (e.yChanged) {
+        //                 // get row
+        //             let idxy = heatmap.dataset.y.nearestIndex(e.realPosition.y);
+        //             let row = heatmap.dataset.data.getRow(idxy);
+        //             linePlot.y = row;
+        //             figx.repaint();
+        //         }
+
+        //         if (e.xChanged) {
+        //             let idxx = heatmap.dataset.x.nearestIndex(e.realPosition.x);
+        //             let col = heatmap.dataset.data.getCol(idxx);
+    
+        //             if (linePlot.x.length !== heatmap.dataset.y.length) {
+        //                 linePlot.x = heatmap.dataset.x;
+        //             }
+        //             linePlotCol.x = heatmap.dataset.y;
+        //             linePlotCol.y = col;
+        //             figy.repaint();
+        //         }
+    
+        //     });
+    
+        //     dLines.linkX(lx);
+        //     dLines.linkYX(ly);
+        //     dLines.positionChanged(true, true);
+        // }
+
+        // this.grid.gridSettings.heightRatios = NumberArray.fromArray([2, 1, 1]);
+
+        // // combinations
+        // for (let i = 0; i < this.figHeatmaps.length - 1; i++) {
+        //     for (let j = 1; j < this.figHeatmaps.length; j++) {
                 
-                this.figHeatmaps[i].linkXRange(this.figHeatmaps[j]);
-                this.figHeatmaps[i].linkYRange(this.figHeatmaps[j]);
+        //         this.figHeatmaps[i].linkXRange(this.figHeatmaps[j]);
+        //         this.figHeatmaps[i].linkYRange(this.figHeatmaps[j]);
 
-                this.dLinesArr[i].linkX(this.dLinesArr[j]);
-                this.dLinesArr[i].linkY(this.dLinesArr[j]);
+        //         this.dLinesArr[i].linkX(this.dLinesArr[j]);
+        //         this.dLinesArr[i].linkY(this.dLinesArr[j]);
 
-            }
-        }
+        //     }
+        // }
 
 
-        this.repaint();
-        this.resize();
+        // this.repaint();
+        // this.resize();
     
             // for (let i = 0; i < dataset.x.length; i++) {
             //     var col = dataset.data.getCol(i);
