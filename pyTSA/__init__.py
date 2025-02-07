bc24949b30569694f44134bb2512b1b28ba13b9a
dominik madea <dominik.madea@seznam.cz>
2025-02-07 17:10:25 +0900

    ...

--- pyTSA/__init__.py
+++ pyTSA/__init__.py
@@ -3,3 +3,4 @@ from .datasets import Datasets
 
 from .kineticmodel import FirstOrderLPLModel, FirstOrderModel, DelayedFluorescenceModel, SensitizationModel
 from .plot import *
+from .mathfuncs import fi, find_nearest
